3f4e628f39b6410856347af06c45b45ea223dfa1
Stenio Oliveira <steniooliv@gmail.com>
2021-07-24 00:34:37 -0300

    create home page

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:fakebucks_flutter/shared/theme/app_colors.dart';
+import 'package:fakebucks_flutter/shared/theme/text_styles.dart';
+import 'package:fakebucks_flutter/src/pages/home/widget/card_home_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,10 +14,77 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            AppBar(
+              elevation: 0,
+              backgroundColor: AppColors.white,
+              centerTitle: true,
+              title: Text(
+                'coffees',
+                style: TextStyles.title,
+              ),
+            ),
+          ],
+        ),
+      ),
       body: Container(
         width: double.maxFinite,
         height: double.maxFinite,
         decoration: BoxDecoration(gradient: AppColors.linearGradient),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              CardHomeWidget(
+                imageName: 'caffè_misto',
+                textTitle: 'Caffè Misto',
+                textCard:
+                    'dlfjalsdjfadslkj alsçdjasdfasdfasdfadsfdsaf ladskjf ladsçj flaç',
+                value: '6.90',
+              ),
+              CardHomeWidget(
+                imageName: 'caffè_misto',
+                textTitle: 'Caffè Misto',
+                textCard:
+                    'dlfjalsdjfadslkj alsçdjasdfasdfasdfadsfdsaf ladskjf ladsçj flaç',
+                value: '6.90',
+              ),
+              CardHomeWidget(
+                imageName: 'caffè_misto',
+                textTitle: 'Caffè Misto',
+                textCard:
+                    'dlfjalsdjfadslkj alsçdjasdfasdfasdfadsfdsaf ladskjf ladsçj flaç',
+                value: '6.90',
+              ),
+              CardHomeWidget(
+                imageName: 'caffè_misto',
+                textTitle: 'Caffè Misto',
+                textCard:
+                    'dlfjalsdjfadslkj alsçdjasdfasdfasdfadsfdsaf ladskjf ladsçj flaç',
+                value: '6.90',
+              ),
+              CardHomeWidget(
+                imageName: 'caffè_misto',
+                textTitle: 'Caffè Misto',
+                textCard:
+                    'dlfjalsdjfadslkj alsçdjasdfasdfasdfadsfdsaf ladskjf ladsçj flaç',
+                value: '6.90',
+              ),
+              CardHomeWidget(
+                imageName: 'caffè_misto',
+                textTitle: 'Caffè Misto',
+                textCard:
+                    'dlfjalsdjfadslkj alsçdjasdfasdfasdfadsfdsaf ladskjf ladsçj flaç',
+                value: '6.90',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
