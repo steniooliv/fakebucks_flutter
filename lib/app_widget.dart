6f6015ba159ad1a3174f194a801f2087c0e83070
Stenio Oliveira <steniooliv@gmail.com>
2021-07-25 01:18:51 -0300

    add splash as home

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,5 +1,3 @@
-import 'package:fakebucks_flutter/src/pages/cart/cart_page.dart';
-import 'package:fakebucks_flutter/src/pages/home/home_page.dart';
 import 'package:fakebucks_flutter/src/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -12,12 +10,7 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      initialRoute: '/splash',
-      routes: {
-        '/splash': (context) => SplashPage(),
-        '/home': (context) => HomePage(),
-        '/cart': (context) => CartPage(),
-      },
+      home: SplashPage(),
     );
   }
 }
